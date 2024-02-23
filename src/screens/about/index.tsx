1684b28dee754fe437f0a537e018565c3c3b11c9
Tatenda Usuwana Mapuranga <tatemapu@gmail.com>
2024-02-24 01:08:46 +1100

    .

--- src/screens/about/index.tsx
+++ src/screens/about/index.tsx
@@ -2,8 +2,13 @@ import { useState } from "react";
 import useWindowSize from "../../hooks/useWindowSize";
 import { Size } from "../../types";
 import portrait from './/tuku.jpg'
+import spotify from './/spotify.png';
+import youtube from './/yt.png';
+import ytmusic from './ytmusic.png';
+import apple from './/apple.png';
 import "./styles.css";
 import { Header } from "../../components/header";
+import Spacer from "react-styled-spacer";
 
 export const About  = () => {
     const size: Size = useWindowSize();
@@ -26,7 +31,17 @@ export const About  = () => {
         <p style={{color: 'aliceblue'}}>Oliver Mtukudzi's significance goes beyond his musical prowess; he became a unifying force in Zimbabwe, fostering a sense of national identity and pride through his compositions. His songs often carried messages of hope, resilience, and the celebration of cultural heritage.</p>
         <p style={{color: 'aliceblue'}}>Internationally, Mtukudzi transcended the boundaries of Zimbabwean music, captivating audiences worldwide with his unique sound. He collaborated with artists from diverse backgrounds, contributing to the global appreciation of African music. His influence extended beyond the music industry, making him a respected ambassador for Zimbabwean culture on the international stage.</p>
         <p style={{color: 'aliceblue'}}>Oliver Mtukudzi's legacy endures not only through his extensive discography but also through the countless lives he touched with his music. He passed away on January 23, 2019, leaving behind a profound impact on the world of music and a lasting imprint on the hearts of Zimbabweans, who continue to celebrate his contributions to their cultural heritage.</p>
+        <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+          <a href="https://music.apple.com/us/artist/oliver-mtukudzi/485646791" target="_blank"><img src={apple} style={{width: '40px'}} className="apple"/></a>
+          <Spacer w={20}/>
+          <a href="https://open.spotify.com/artist/0HC2dfJHpORLT21AxEiTXJ" target="_blank"><img src={spotify} style={{width: '40px'}} className="spotify"/></a>
+          <Spacer w={20}/>
+          <a href="https://music.youtube.com/channel/UCVoeijjk8ewIn3CZag6MCiQ" target="_blank"><img src={ytmusic} style={{width: '40px'}} className="ytmusic"/></a>
+          <Spacer w={20}/>
+          <a href="https://www.youtube.com/channel/UC-K797_lLK1QjhkXsWzMXNQ" target="_blank"><img src={youtube} style={{width: '40px'}} className="youtube"/></a>
         </div>
+        </div>
+        
         
       </div>
       </div>
