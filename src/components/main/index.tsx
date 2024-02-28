2c42ac736fc2aee1a8d551a1200c8e134f104ae4
Tatenda Usuwana Mapuranga <tatemapu@gmail.com>
2024-02-28 21:55:05 +1100

    .

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -1,27 +1,106 @@
 import Tuku from ".//tukumain.png"
 import "./styles.css";
+import Typewriter from 'typewriter-effect';
+import { TypeAnimation } from 'react-type-animation';
 
 export const Main  = () => {
     return (
     <div className="main-row">
         <div className="main-column">
-            <div>
-            <p style={{color: 'aliceblue', padding: '10px', textAlign: 'right'}}>If you have a word, just say it loud. Say it from your heart. Cause you have to mean what you sing. Say what you mean and you got to sing what you mean, mean what you sing.</p>
+
+            <div style={{color: 'aliceblue', paddingTop: '50px'}}>
+                <TypeAnimation
+                    sequence={[
+                    `If you have a word, just say it loud. Say it from your heart. Cause you have to mean what you sing. Say what you mean and you got to sing what you mean, mean what you sing...`,
+                    1000,
+                    "",
+                    ]}
+                    
+                    style={{ whiteSpace: 'pre-line', fontSize: '1em', color: 'aliceblue' }}
+                    repeat={Infinity}
+                    speed={1}
+                    deletionSpeed={1}
+                    cursor={false}
+                />
             </div>
             <div className="left-column-one">
-            <p style={{color: 'aliceblue'}}>Hatikanganwe varere mumasango. Vasina makuva. Vakafira nhaka yavo. Vakafira nyika yavo iyi.</p>
+            
+            <TypeAnimation
+                    sequence={[
+                    `Hatikanganwe varere mumasango. Vasina makuva. Vakafira nhaka yavo. Vakafira nyika yavo iyi...`,
+                    1000,
+                    "",
+                    ]}
+                    
+                    style={{ whiteSpace: 'pre-line', fontSize: '1em', color: 'aliceblue' }}
+                    repeat={Infinity}
+                    speed={1}
+                    deletionSpeed={1}
+                    cursor={false}
+                />
             </div>
             <div className="left-column-one">
-            <p style={{color: 'aliceblue'}}>Mwana asingacheme anofira mumbereko.</p>
+            <TypeAnimation
+                    sequence={[
+                    `Mwana asingacheme anofira mumbereko...`,
+                    1000,
+                    "",
+                    ]}
+                    
+                    style={{ whiteSpace: 'pre-line', fontSize: '1em', color: 'aliceblue' }}
+                    repeat={Infinity}
+                    speed={1}
+                    deletionSpeed={1}
+                    cursor={false}
+                />
             </div>
             <div className="left-column-one">
-            <p style={{color: 'aliceblue'}}>Changu chiiko, nhai Mwari? Changu chii?</p>
+            
+            <TypeAnimation
+                    sequence={[
+                    `Changu chiiko, nhai Mwari? Changu chii?`,
+                    1000,
+                    "",
+                    ]}
+                    
+                    style={{ whiteSpace: 'pre-line', fontSize: '1em', color: 'aliceblue' }}
+                    repeat={Infinity}
+                    speed={1}
+                    deletionSpeed={1}
+                    cursor={false}
+                />
             </div> 
             <div className="left-column-one">
-            <p style={{color: 'aliceblue'}}>Tozeza baba. Baba chidhakwa.</p>
+            
+            <TypeAnimation
+                    sequence={[
+                    `Tozeza baba. Baba chidhakwa...`,
+                    1000,
+                    "",
+                    ]}
+                    
+                    style={{ whiteSpace: 'pre-line', fontSize: '1em', color: 'aliceblue' }}
+                    repeat={Infinity}
+                    speed={1}
+                    deletionSpeed={1}
+                    cursor={false}
+                />
             </div>
             <div className="left-column-one">
-            <p style={{color: 'aliceblue'}}>Ishe taungana muno mumba menyu. Huyanyiwo izvo tapinda Tenzi mberi kwenyu. Iyo moyo yedu inomuchemera. Neyi mweya yedu Tenzi tinokutarira.</p>
+            
+            <TypeAnimation
+                    sequence={[
+                    `Ishe taungana muno mumba menyu. Huyanyiwo izvo tapinda Tenzi mberi kwenyu. Iyo moyo yedu inomuchemera. Neyi mweya yedu Tenzi tinokutarira...`,
+                    1000,
+                    "",
+                    ]}
+                    
+                    style={{ whiteSpace: 'pre-line', fontSize: '1em', color: 'aliceblue' }}
+                    repeat={Infinity}
+                    speed={1}
+                    deletionSpeed={1}
+                    cursor={false}
+                />
         </div>
         </div>
        
@@ -33,23 +112,101 @@ export const Main  = () => {
             
         </div>
         <div className="left-column-one" style={{paddingTop: '70px'}}>
-            <p style={{color: 'aliceblue'}}>Ndoshandira mhuri yangu Ndoshandira mhuri yangu varume Ndoshandira kurarama Ndoshandira kurarama vakomana.</p>
+            
+            <TypeAnimation
+                    sequence={[
+                    `Ndoshandira mhuri yangu Ndoshandira mhuri yangu varume Ndoshandira kurarama Ndoshandira kurarama vakomana...`,
+                    1000,
+                    "",
+                    ]}
+                    
+                    style={{ whiteSpace: 'pre-line', fontSize: '1em', color: 'aliceblue' }}
+                    repeat={Infinity}
+                    speed={1}
+                    deletionSpeed={1}
+                    cursor={false}
+                />
         </div>
         
         <div className="left-column-one">
-            <p style={{color: 'aliceblue'}}>Usakanganwe kwawakabva, kwaunobva.</p>
+            
+            <TypeAnimation
+                    sequence={[
+                    `Usakanganwe kwawakabva, kwaunobva...`,
+                    1000,
+                    "",
+                    ]}
+                    
+                    style={{ whiteSpace: 'pre-line', fontSize: '1em', color: 'aliceblue' }}
+                    repeat={Infinity}
+                    speed={1}
+                    deletionSpeed={1}
+                    cursor={false}
+                />
             </div>
             <div className="left-column-one">
-            <p style={{color: 'aliceblue'}}>Hallo hallo hallo my pal. How come you loo so sad? You used to be hard. To be smart, too smart to work. Now that chance is gone, and its late to recover.</p>
+            <p style={{color: 'aliceblue'}}></p>
+            <TypeAnimation
+                    sequence={[
+                    `Hallo hallo hallo my pal. How come you loo so sad? You used to be hard. To be smart, too smart to work. Now that chance is gone, and its late to recover...`,
+                    1000,
+                    "",
+                    ]}
+                    
+                    style={{ whiteSpace: 'pre-line', fontSize: '1em', color: 'aliceblue' }}
+                    repeat={Infinity}
+                    speed={1}
+                    deletionSpeed={1}
+                    cursor={false}
+                />
             </div>
             <div className="left-column-one">
-            <p style={{color: 'aliceblue'}}>Ndawana wekuchemera mambo. Ndawana anondichengeta. Ndawana wekuturira mambo. Nekuyemera.</p>
+            
+            <TypeAnimation
+                    sequence={[
+                    `Ndawana wekuchemera mambo. Ndawana anondichengeta. Ndawana wekuturira mambo. Nekuyemera...`,
+                    1000,
+                    "",
+                    ]}
+                    
+                    style={{ whiteSpace: 'pre-line', fontSize: '1em', color: 'aliceblue' }}
+                    repeat={Infinity}
+                    speed={1}
+                    deletionSpeed={1}
+                    cursor={false}
+                />
         </div>
         <div className="left-column-one">
-            <p style={{color: 'aliceblue'}}>Ndinoti seiko, seiko Mwari nhai? Nhai Mwari seiko, seiko Mwari iwe? Kwandiri seiko?</p>
+            
+            <TypeAnimation
+                    sequence={[
+                    `Ndinoti seiko, seiko Mwari nhai? Nhai Mwari seiko, seiko Mwari iwe? Kwandiri seiko?`,
+                    1000,
+                    "",
+                    ]}
+                    
+                    style={{ whiteSpace: 'pre-line', fontSize: '1em', color: 'aliceblue' }}
+                    repeat={Infinity}
+                    speed={1}
+                    deletionSpeed={1}
+                    cursor={false}
+                />
         </div>
         <div className="left-column-one">
-            <p style={{color: 'aliceblue'}}>Ndine mubvunzo mumwechete Mwari undiudze. Ndoda kuti undiudze pandimire chaipo. Saka ndati seiko seiko baba haa?</p>
+        
+            <TypeAnimation
+                    sequence={[
+                    `Ndine mubvunzo mumwechete Mwari undiudze. Ndoda kuti undiudze pandimire chaipo. Saka ndati seiko seiko baba haa?`,
+                    1000,
+                    "",
+                    ]}
+                    
+                    style={{ whiteSpace: 'pre-line', fontSize: '1em', color: 'aliceblue' }}
+                    repeat={Infinity}
+                    speed={1}
+                    deletionSpeed={1}
+                    cursor={false}
+                />
         </div>
         </div>
         <div className="main-column">
@@ -57,51 +214,219 @@ export const Main  = () => {
             
         </div>
         <div className="left-column-one">
-            <p style={{color: 'aliceblue'}}>Shoko rakafamba, ndokufamba rikafamba.</p>
+            
+            <TypeAnimation
+                    sequence={[
+                    `Shoko rakafamba, ndokufamba rikafamba...`,
+                    1000,
+                    "",
+                    ]}
+                    
+                    style={{ whiteSpace: 'pre-line', fontSize: '1em', color: 'aliceblue' }}
+                    repeat={Infinity}
+                    speed={1}
+                    deletionSpeed={1}
+                    cursor={false}
+                />
             </div>
             <div className="left-column-one">
-            <p style={{color: 'aliceblue'}}>Hazvireve rudo handina ini
-Hazvireve rudo handina mwanangu iwe
-Rudo ndinarwo mwanangu.</p>
+            
+            <TypeAnimation
+                    sequence={[
+                    `Hazvireve rudo handina ini
+                    Hazvireve rudo handina mwanangu iwe
+                    Rudo ndinarwo mwanangu...`,
+                    1000,
+                    "",
+                    ]}
+                    speed={1}
+                    deletionSpeed={1}
+                    cursor={false}
+                    style={{ whiteSpace: 'pre-line', fontSize: '1em', color: 'aliceblue' }}
+                    repeat={Infinity}
+                />
             </div>
         <div className="left-column-one" style={{paddingTop: '10px'}}>
-            <p style={{color: 'aliceblue'}}>Do you have to die to be a hero, a national hero, here?</p>
+            
+            <TypeAnimation
+                    sequence={[
+                    `Do you have to die to be a hero, a national hero, here?`,
+                    1000,
+                    "",
+                    ]}
+                    speed={1}
+                    deletionSpeed={1}
+                    cursor={false}
+                    style={{ whiteSpace: 'pre-line', fontSize: '1em', color: 'aliceblue' }}
+                    repeat={Infinity}
+                />
         </div>
         <div className="left-column-one" style={{paddingTop: '10px', }}>
-        <p style={{color: 'aliceblue'}}>Wongorora chikonzero chaita musoro uteme. Ugogadzirisa chikonzero chaita musana ubande.</p>
+        <TypeAnimation
+                    sequence={[
+                    `Wongorora chikonzero chaita musoro uteme. Ugogadzirisa chikonzero chaita musana ubande...`,
+                    1000,
+                    "",
+                    ]}
+                    speed={1}
+                    deletionSpeed={1}
+                    cursor={false}
+                    style={{ whiteSpace: 'pre-line', fontSize: '1em', color: 'aliceblue' }}
+                    repeat={Infinity}
+                />
         </div>
         <div className="left-column-one">
-            <p style={{color: 'aliceblue'}}>Together we can make a difference. Let's get together and pray for the nation.</p>
+            
+            <TypeAnimation
+                    sequence={[
+                    `Together we can make a difference. Let's get together and pray for the nation...`,
+                    1000,
+                    "",
+                    ]}
+                    speed={1}
+                    deletionSpeed={1}
+                    cursor={false}
+                    style={{ whiteSpace: 'pre-line', fontSize: '1em', color: 'aliceblue' }}
+                    repeat={Infinity}
+                />
             </div>
             <div className="left-column-one">
-            <p style={{color: 'aliceblue'}}>Ndati hope hadzina ndima, hadzina ndima. Ukagudugudza wasaririra. Apa basa mangwanani, mangwanani</p>
+            
+            <TypeAnimation
+                    sequence={[
+                    `Ndati hope hadzina ndima, hadzina ndima. Ukagudugudza wasaririra. Apa basa mangwanani, mangwanani...`,
+                    1000,
+                    "",
+                    ]}
+                    speed={1}
+                    deletionSpeed={1}
+                    cursor={false}
+                    style={{ whiteSpace: 'pre-line', fontSize: '1em', color: 'aliceblue' }}
+                    repeat={Infinity}
+                />
             </div>
             <div className="left-column-one">
-            <p style={{color: 'aliceblue'}}>Ndovayemura ende baba vakaenda. Ndovayemura ava tete vakafa kare hinde. Hupenyu hwavo waiva mutserendende.</p>
+            
+            <TypeAnimation
+                    sequence={[
+                    `Ndovayemura ende baba vakaenda. Ndovayemura ava tete vakafa kare hinde. Hupenyu hwavo waiva mutserendende...`,
+                    1000,
+                    "",
+                    ]}
+                    speed={1}
+                    deletionSpeed={1}
+                    cursor={false}
+                    style={{ whiteSpace: 'pre-line', fontSize: '1em', color: 'aliceblue' }}
+                    repeat={Infinity}
+                />
         </div>
             </div>
         <div className="main-column">
         <div className="left-column-one">
-            <p style={{color: 'aliceblue'}}>Mwana wamambo, muranda kumwe.</p>
+            
+            <TypeAnimation
+                    sequence={[
+                    `Mwana wamambo, muranda kumwe...`,
+                    1000,
+                    "",
+                    ]}
+                    speed={1}
+                    deletionSpeed={1}
+                    cursor={false}
+                    style={{ whiteSpace: 'pre-line', fontSize: '1em', color: 'aliceblue' }}
+                    repeat={Infinity}
+                />
         </div>
             
             <div className="left-column-one">
-            <p style={{color: 'aliceblue'}}>Kufirwa nemurume hanzvanzi, zvinoda moyo wekushinga. Usaore moyokaNeria, Mwari anewe.</p>
+            
+            <TypeAnimation
+                    sequence={[
+                    `Kufirwa nemurume hanzvanzi, zvinoda moyo wekushinga. Usaore moyokaNeria, Mwari anewe...`,
+                    1000,
+                    "",
+                    ]}
+                    speed={1}
+                    deletionSpeed={1}
+                    cursor={false}
+                    style={{ whiteSpace: 'pre-line', fontSize: '1em', color: 'aliceblue' }}
+                    repeat={Infinity}
+                />
         </div>
         <div className="left-column-one">
-            <p style={{color: 'aliceblue'}}>What you do in the dark can be known in a day. Rinemanyanga hariputirwe. What you think is a secret can be a gossip everywhere.</p>
+            
+            <TypeAnimation
+                    sequence={[
+                    `What you do in the dark can be known in a day. Rinemanyanga hariputirwe. What you think is a secret can be a gossip everywhere...`,
+                    1000,
+                    "",
+                    ]}
+                    speed={1}
+                    deletionSpeed={1}
+                    cursor={false}
+                    style={{ whiteSpace: 'pre-line', fontSize: '1em', color: 'aliceblue' }}
+                    repeat={Infinity}
+                />
         </div>
         <div className="left-column-one">
-            <p style={{color: 'aliceblue'}}>Zuva nezuva haritonge matare ngoromera.</p>
+            
+            <TypeAnimation
+                    sequence={[
+                    `Zuva nezuva haritonge matare ngoromera...`,
+                    1000,
+                    "",
+                    ]}
+                    speed={1}
+                    deletionSpeed={1}
+                    cursor={false}
+                    style={{ whiteSpace: 'pre-line', fontSize: '1em', color: 'aliceblue' }}
+                    repeat={Infinity}
+                />
         </div>
         <div className="left-column-one">
-            <p style={{color: 'aliceblue'}}>Ruzivo zvapararira inga wani. Njere dzapararira wona iwe</p>
+            
+            <TypeAnimation
+                    sequence={[
+                    `Ruzivo zvapararira inga wani. Njere dzapararira wona iwe...`,
+                    1000,
+                    "",
+                    ]}
+                    speed={1}
+                    deletionSpeed={1}
+                    cursor={false}
+                    style={{ whiteSpace: 'pre-line', fontSize: '1em', color: 'aliceblue' }}
+                    repeat={Infinity}
+                />
         </div>
         <div className="left-column-one">
-            <p style={{color: 'aliceblue'}}>Varipo varume vanorarama neraki. Iripo mikono inorarama neraki. Isingazive pane anochengeta.</p>
+            
+            <TypeAnimation
+                    sequence={[
+                    `Varipo varume vanorarama neraki. Iripo mikono inorarama neraki. Isingazive pane anochengeta...`,
+                    1000,
+                    "",
+                    ]}
+                    speed={1}
+                    deletionSpeed={1}
+                    cursor={false}
+                    style={{ whiteSpace: 'pre-line', fontSize: '1em', color: 'aliceblue' }}
+                    repeat={Infinity}
+                />
         </div>
         <div className="left-column-one">
-            <p style={{color: 'aliceblue'}}>Zvasariresu vapenyu. Isu vapenyu, kufuga nekuwarira. Tozadza mikombe zvichida tichazosangana.</p>
+            
+            <TypeAnimation
+                    sequence={[
+                    `Zvasariresu vapenyu. Isu vapenyu, kufuga nekuwarira. Tozadza mikombe zvichida tichazosangana...`,
+                    1000,
+                    "",
+                    ]}
+                    speed={1}
+                    deletionSpeed={1}
+                    cursor={false}
+                    style={{ whiteSpace: 'pre-line', fontSize: '1em', color: 'aliceblue' }}
+                    repeat={Infinity}
+                />
         </div>
         </div>
         
